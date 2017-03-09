34791c9ae972856fc453f0b5715c8074df9f92b2
Ivan Kochurkin <kvanttt@gmail.com>
2017-03-09 15:54:31 +0300

    Remove all grammars.

--- Sources/antlr-grammars-v4/php/examples/numericScale.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-$a = 1234;       // decimal
-$b = 0123;       // octal (83 in decimal)
-$c = 0x1A;       // hex (26 in decimal)
-$d = 0b11111111; // binary (255 in decimal)
-$e = 123.4;      // float
