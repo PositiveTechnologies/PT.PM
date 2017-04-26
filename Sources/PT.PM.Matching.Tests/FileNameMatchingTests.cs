341299d6a0bd9b731cdf05f67ef34d316516cd5f
Ivan Kochurkin <kvanttt@gmail.com>
2017-04-26 17:58:59 +0300

    Added FileNameMatcher unit-tests.

---
+++ Sources/PT.PM.Matching.Tests/FileNameMatchingTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace PT.PM.Matching.Tests
+{
+    [TestFixture]
+    public class FileNameMatchingTests
+    {
+        private const string TestFileName = "C:/Users/Unnamed/examples/sample.php";
+
+        [TestCase(TestFileName)]
+        [TestCase("**/examples/*")]
+        [TestCase("**/Users/**/examples/*")]
+        [TestCase("**/Users/**/examples/*.php")]
+        [TestCase("*.php")]
+        [TestCase("*.*")]
+        public void MatchWildcard(string wildcard)
+        {
+            var pathMatcher = new FileNameMatcher();
+            var regex = pathMatcher.WildcardToRegex(wildcard);
+            Assert.IsTrue(regex.IsMatch(TestFileName));
+        }
+
+        [TestCase("**/Temp/**/*")]
+        [TestCase("**/Unnamed/**/examples/*.txt")]
+        [TestCase("*.txt")]
+        public void MatchWildcard_NotMatched(string wildcard)
+        {
+            var pathMatcher = new FileNameMatcher();
+            var regex = pathMatcher.WildcardToRegex(wildcard);
+            Assert.IsFalse(regex.IsMatch(TestFileName));
+        }
+
+        [TestCase("**/USERS/**/EXAMPLES/*")]
+        public void MatchWildcard_CaseInsensitive(string wildcard)
+        {
+            var pathMatcher = new FileNameMatcher() { IsCaseInsensitive = true };
+            var regex = pathMatcher.WildcardToRegex(wildcard);
+            Assert.IsTrue(regex.IsMatch(TestFileName));
+        }
+    }
+}
