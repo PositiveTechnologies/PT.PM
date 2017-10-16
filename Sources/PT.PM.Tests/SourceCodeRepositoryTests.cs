2ccd8bdf95340f5514d79b2e13f345083844c75d
Ivan Kochurkin <kvanttt@gmail.com>
2017-10-16 21:29:07 +0300

    Added Check_AspxFileWithCSharpLanguage_NotIgnored unit-test.

--- Sources/PT.PM.Tests/SourceCodeRepositoryTests.cs
+++ Sources/PT.PM.Tests/SourceCodeRepositoryTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using PT.PM.Common.CodeRepository;
 using PT.PM.TestUtils;
 using NUnit.Framework;
+using PT.PM.CSharpParseTreeUst;
 
 namespace PT.PM.Tests
 {
@@ -12,12 +13,20 @@ namespace PT.PM.Tests
         [Test]
         public void AggregateFiles_TestProject_CorrectCountAndRelativePaths()
         {
-            var repository = new FilesAggregatorCodeRepository(Path.Combine(TestUtility.TestsDataPath, "Test Project"), ".cs");
+            var repository = new FilesAggregatorCodeRepository(Path.Combine(TestUtility.TestsDataPath, "Test Project"), CSharp.Language);
             var fileNames = repository.GetFileNames().Select(fileName => repository.ReadFile(fileName)).ToArray();
 
             Assert.AreEqual(7, fileNames.Length);
             Assert.IsNotNull(fileNames.SingleOrDefault(f => f.Name == "1.cs" && f.RelativePath == ""));
             Assert.IsNotNull(fileNames.SingleOrDefault(f => f.Name == "1.cs" && f.RelativePath == "Folder"));
+        }
+
+        [Test]
+        public void Check_AspxFileWithCSharpLanguage_NotIgnored()
+        {
+            var repository = new FilesAggregatorCodeRepository("", CSharp.Language);
+
+            Assert.IsFalse(repository.IsFileIgnored("page.aspx"));
         }
     }
 }
