d12e86eeb047782add6ad09244496251a6f47bcf
Ivan Kochurkin <kvanttt@gmail.com>
2017-10-26 15:43:33 +0300

    Fix build.

--- Sources/PT.PM.Common/LanguageUtils.cs
+++ Sources/PT.PM.Common/LanguageUtils.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using static System.Collections.Generic.Dictionary<TKey, TValue>;
 
 namespace PT.PM.Common
 {
