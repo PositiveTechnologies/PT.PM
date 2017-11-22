f6a39d6f2a94b7cd56eb40fc96773a4d7ae9cd22
Ivan Kochurkin <kvanttt@gmail.com>
2017-11-22 19:01:56 +0300

    Added LanguageJsonConverter.

---
+++ Sources/PT.PM.Common/Json/LanguageJsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PT.PM.Common.Json
+{
+    public class LanguageJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Language);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((Language)value).Key);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return LanguageUtils.Languages[(string)reader.Value];
+        }
+    }
+}
