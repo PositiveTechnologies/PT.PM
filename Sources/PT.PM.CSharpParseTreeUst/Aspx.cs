2cdac79a816a96ed447058974c4d9a3ad95d5874
Ivan Kochurkin <kvanttt@gmail.com>
2017-12-19 00:36:01 +0300

    Removed HaveAntlrParser flag from Aspx language.

--- Sources/PT.PM.CSharpParseTreeUst/Aspx.cs
+++ Sources/PT.PM.CSharpParseTreeUst/Aspx.cs
@@ -5,6 +5,7 @@ namespace PT.PM.CSharpParseTreeUst
     public static class Aspx
     {
         public readonly static Language Language =
-            new Language("Aspx", new[] { ".asax", ".aspx", ".ascx", ".master" }, false, "Aspx", new[] { CSharp.Language }, isPattern: false);
+            new Language("Aspx", new[] { ".asax", ".aspx", ".ascx", ".master" },
+                false, "Aspx", new[] { CSharp.Language }, false, false);
     }
 }
