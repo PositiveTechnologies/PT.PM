85fadc148a9857f32803728142fc3da3d9019948
Ivan Kochurkin <kvanttt@gmail.com>
2018-01-13 03:04:02 +0300

    Fixed TextSpans for C# conversion.

--- Sources/PT.PM.CSharpParseTreeUst/Converter/ExpressionVisitor.cs
+++ Sources/PT.PM.CSharpParseTreeUst/Converter/ExpressionVisitor.cs
@@ -12,7 +12,6 @@ using PT.PM.Common;
 using PT.PM.Common.Exceptions;
 using System.Collections.Generic;
 using PT.PM.Common.Nodes.Tokens.Literals;
-//using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace PT.PM.CSharpParseTreeUst.RoslynUstVisitor
 {
