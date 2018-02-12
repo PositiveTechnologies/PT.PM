9bfaabacf6043477b685b76a70a5377e6505e6c5
Ivan Kochurkin <kvanttt@gmail.com>
2018-02-12 13:39:53 +0300

    Added CombiningPatternsRepository.

---
+++ Sources/PT.PM.Matching/PatternsRepository/CombiningPatternsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace PT.PM.Matching.PatternsRepository
+{
+    public class CombiningPatternsRepository : MemoryPatternsRepository
+    {
+        private IPatternsRepository[] patternsRepositories;
+
+        public CombiningPatternsRepository(params IPatternsRepository[] patternsRepositories)
+        {
+            this.patternsRepositories = patternsRepositories;
+        }
+
+        protected override List<PatternDto> InitPatterns()
+        {
+            var result = new List<PatternDto>();
+            foreach (IPatternsRepository patternRepository in patternsRepositories)
+            {
+                result.AddRange(patternRepository.GetAll());
+            }
+            return result;
+        }
+    }
+}
