2593e038fe0fe2194984f48552a5d2c975171b8a
Ivan Kochurkin <kvanttt@gmail.com>
2018-02-14 13:41:46 +0300

    ConstructorDeclaration inherited from MethodDeclaration.

--- Sources/PT.PM.Common/Nodes/TypeMembers/ConstructorDeclaration.cs
+++ Sources/PT.PM.Common/Nodes/TypeMembers/ConstructorDeclaration.cs
@@ -1,34 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using PT.PM.Common.Nodes.Statements;
 using PT.PM.Common.Nodes.Tokens;
-using PT.PM.Common.Nodes.Statements;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace PT.PM.Common.Nodes.TypeMembers
 {
-    public class ConstructorDeclaration : EntityDeclaration
+    public class ConstructorDeclaration : MethodDeclaration
     {
-        public List<ParameterDeclaration> Args { get; set; }
-
-        public BlockStatement Body { get; set; }
-
-        public ConstructorDeclaration(IdToken typeName, IEnumerable<ParameterDeclaration> args, BlockStatement body,
+        public ConstructorDeclaration(IdToken typeName, IEnumerable<ParameterDeclaration> parameters, BlockStatement body,
             TextSpan textSpan)
-            : base(typeName, textSpan)
+            : base(typeName, parameters, body, textSpan)
         {
-            Args = args as List<ParameterDeclaration> ?? args.ToList();
-            Body = body;
         }
 
         public ConstructorDeclaration()
         {
-        }
-
-        public override Ust[] GetChildren()
-        {
-            var result = new List<Ust>(base.GetChildren());
-            result.AddRange(Args);
-            result.Add(Body);
-            return result.ToArray();
         }
     }
 }
