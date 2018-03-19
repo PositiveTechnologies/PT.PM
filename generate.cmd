f8012e23c8180997d6c4422586480758993490b4
Ivan Kochurkin <kvanttt@gmail.com>
2018-03-19 18:43:17 +0300

    Split PT.PM.SqlParseTreeUst project on PT.PM.PlSqlParseTreeUst and PT.PM.TSqlParseTreeUst.
    Fixed PT.PM.sln.

--- generate.cmd
+++ generate.cmd
@@ -6,6 +6,6 @@ dotnet AntlrGen/AntlrGen.dll --lexer Sources/antlr-grammars-v4/javascript/JavaSc
 
 dotnet AntlrGen/AntlrGen.dll --lexer Sources/antlr-grammars-v4/php/CSharpSharwell/PhpLexer.g4 --parser Sources/antlr-grammars-v4/php/PhpParser.g4 --package PT.PM.PhpParseTreeUst --output Sources/PT.PM.PhpParseTreeUst/Generated
 
-dotnet AntlrGen/AntlrGen.dll --lexer Sources/antlr-grammars-v4/plsql/PlSqlLexer.g4 --parser Sources/antlr-grammars-v4/plsql/PlSqlParser.g4 --package PT.PM.PlSqlParseTreeUst --output Sources/PT.PM.SqlParseTreeUst/Generated
+dotnet AntlrGen/AntlrGen.dll --lexer Sources/antlr-grammars-v4/plsql/PlSqlLexer.g4 --parser Sources/antlr-grammars-v4/plsql/PlSqlParser.g4 --package PT.PM.PlSqlParseTreeUst --output Sources/PT.PM.PlSqlParseTreeUst/Generated
 
-dotnet AntlrGen/AntlrGen.dll --lexer Sources/antlr-grammars-v4/tsql/TSqlLexer.g4 --parser Sources/antlr-grammars-v4/tsql/TSqlParser.g4 --package PT.PM.TSqlParseTreeUst --output Sources/PT.PM.SqlParseTreeUst/Generated
+dotnet AntlrGen/AntlrGen.dll --lexer Sources/antlr-grammars-v4/tsql/TSqlLexer.g4 --parser Sources/antlr-grammars-v4/tsql/TSqlParser.g4 --package PT.PM.TSqlParseTreeUst --output Sources/PT.PM.TSqlParseTreeUst/Generated
