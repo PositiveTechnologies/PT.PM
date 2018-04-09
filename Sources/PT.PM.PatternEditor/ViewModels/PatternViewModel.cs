2e43374b2325e1509a7e9fd0b89adefb28303fb9
Dmitry Rassadin <flipparassa@gmail.com>
2018-04-09 10:47:08 +0300

    Tiny fix for Pt.PM.Gui compilation.

--- Sources/PT.PM.PatternEditor/ViewModels/PatternViewModel.cs
+++ Sources/PT.PM.PatternEditor/ViewModels/PatternViewModel.cs
@@ -471,7 +471,7 @@ namespace PT.PM.PatternEditor
                     PatternErrorsText = "";
                     if (IsDeveloperMode && patternNode != null)
                     {
-                        jsonPatternSerializer.CodeFiles = new List<CodeFile>() { patternNode.CodeFile };
+                        jsonPatternSerializer.CodeFiles = new HashSet<CodeFile>() { patternNode.CodeFile };
                         jsonPatternSerializer.CurrectCodeFile = patternNode.CodeFile;
                         PatternJson = jsonPatternSerializer.Serialize(patternNode);
                         File.WriteAllText(Path.Combine(ServiceLocator.TempDirectory, "Pattern UST.json"), PatternJson);
