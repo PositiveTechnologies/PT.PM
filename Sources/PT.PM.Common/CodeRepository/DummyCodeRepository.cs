0f8b3c9cff3772a6445836fa2c8094cc3f58ea20
Ivan Kochurkin <kvanttt@gmail.com>
2018-04-18 19:01:33 +0300

    Added DummyCodeRepository.

---
+++ Sources/PT.PM.Common/CodeRepository/DummyCodeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PT.PM.Common.CodeRepository
+{
+    public class DummyCodeRepository : SourceCodeRepository
+    {
+        public override IEnumerable<string> GetFileNames() => ArrayUtils<string>.EmptyArray;
+
+        public override bool IsFileIgnored(string fileName) => throw new InvalidOperationException("Should not be called");
+
+        public override CodeFile ReadFile(string fileName) => throw new InvalidOperationException("Should not be called");
+    }
+}
