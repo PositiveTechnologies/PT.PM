ea6cc55090f14d0d0b2a293a5185581a05e705f6
Ivan Kochurkin <kvanttt@gmail.com>
2018-04-18 19:57:20 +0300

    Added DummyPatternsRepository.

---
+++ Sources/PT.PM.Matching/PatternsRepository/DummyPatternsRepository.cs
@@ -0,0 +1,21 @@
+﻿using PT.PM.Common;
+using System;
+using System.Collections.Generic;
+
+namespace PT.PM.Matching.PatternsRepository
+{
+    public class DummyPatternsRepository : IPatternsRepository
+    {
+        public string Path { get; set; } = "";
+
+        public IEnumerable<string> Identifiers { get; set; } = ArrayUtils<string>.EmptyArray;
+
+        public ILogger Logger { get; set; } = DummyLogger.Instance;
+
+        public void Add(IEnumerable<PatternDto> pattern) => throw new InvalidOperationException("Should not be called");
+
+        public void Clear() => throw new InvalidOperationException("Should not be called");
+
+        public IEnumerable<PatternDto> GetAll() => ArrayUtils<PatternDto>.EmptyArray;
+    }
+}
