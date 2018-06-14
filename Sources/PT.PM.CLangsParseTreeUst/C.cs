373cfc6a78ba76b1d15ed23b57eada722658f026
Dmitry Rassadin <flipparassa@gmail.com>
2018-06-14 14:44:12 +0300

    Added CLangs project.

---
+++ Sources/PT.PM.CLangsParseTreeUst/C.cs
@@ -0,0 +1,10 @@
+﻿using PT.PM.Common;
+
+namespace PT.PM.CLangsParseTreeUst
+{
+    public static class C
+    {
+        public readonly static Language Language =
+            new Language("C", new[] { ".c", ".h" }, false, "C");
+    }
+}
---
+++ Sources/PT.PM.CLangsParseTreeUst/Cplusplus.cs
@@ -0,0 +1,10 @@
+﻿using PT.PM.Common;
+
+namespace PT.PM.CLangsParseTreeUst
+{
+    public static class Cplusplus
+    {
+        public readonly static Language Language =
+            new Language("Cplusplus", new[] { ".cpp", ".hpp", ".cc", ".cxx" }, false, "Cplusplus", new[] { C.Language });
+    }
+}
