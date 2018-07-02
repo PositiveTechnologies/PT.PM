19221f1c5ea55bffa96a23ab1bc87217bc9a9e36
Ivan Kochurkin <kvanttt@gmail.com>
2018-07-02 17:58:57 +0300

    Added Message to ProgressEventArgs.

--- Sources/PT.PM.Common/ProgressEventArgs.cs
+++ Sources/PT.PM.Common/ProgressEventArgs.cs
@@ -4,20 +4,25 @@ namespace PT.PM.Common
 {
     public class ProgressEventArgs : EventArgs
     {
-        public double Progress { get; set; }
+        public double Progress { get; }
 
-        public string CurrentFile { get; set; }
+        public string CurrentFile { get; }
 
-        public ProgressEventArgs(double progress, string fileName)
+        public string Message { get; }
+
+        public ProgressEventArgs(double progress, string fileName, string message = "")
         {
             Progress = progress;
             CurrentFile = fileName;
+            Message = message;
         }
 
         public override string ToString()
         {
             string value = Progress > 1 ? $"{(int)Progress} items" : $"{Progress*100:0.00}%";
-            return $"Progress: {value}; File: {CurrentFile}";
+            string messageStr = string.IsNullOrEmpty(Message) ? "" : $"; {Message}";
+            string fileStr = string.IsNullOrEmpty(CurrentFile) ? "" : $"; File: {CurrentFile}";
+            return $"Progress: {value}{messageStr}{fileStr}";
         }
     }
 }
