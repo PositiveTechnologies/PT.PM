b37c2d5217f92455b16c23d1acb6b432110706d3
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2018-07-12 21:40:29 +0300

    Add Regex to PatternAny, if exists and node is IdToken, try to match

--- Sources/PT.PM.Matching/Patterns/PatternAny.cs
+++ Sources/PT.PM.Matching/Patterns/PatternAny.cs
@@ -1,19 +1,57 @@
-﻿using PT.PM.Common.Nodes;
+﻿using System.Text.RegularExpressions;
+using PT.PM.Common.Nodes;
+using PT.PM.Common.Nodes.Tokens;
 
 namespace PT.PM.Matching.Patterns
 {
-    public class PatternAny : PatternUst<Ust>
+    public class PatternAny : PatternUst<Ust>, IRegexPattern
     {
         public static PatternAny Instance = new PatternAny();
 
+        public string Default => ".*";
+
+        public string RegexString
+        {
+            get => Regex.ToString();
+            set => Regex = new Regex(string.IsNullOrEmpty(value) ? Default : value, RegexOptions.Compiled);
+        }
+
+        public Regex Regex { get; private set; }
+
+        public override bool Any => Regex == null || RegexString.Equals(Default);
+
+        public PatternAny() { }
+
+        public PatternAny(string regex)
+        {
+            RegexString = regex;
+        }
+
         public override MatchContext Match(Ust ust, MatchContext context)
         {
-            if (ust == null)
+            if (Any)
             {
-                return context.MakeSuccess();
+                if (ust == null)
+                {
+                    return context.MakeSuccess();
+                }
+                return context.AddMatch(ust);
             }
 
-            return context.AddMatch(ust);
+            if (ust is IdToken idUst)
+            {
+                var match = Regex.Match(idUst.Id);
+                if (match.Success)
+                {
+                    return context.AddMatch(idUst);
+                }
+            }
+            return context.Fail();
+        }
+
+        public override string ToString()
+        {
+            return $@"<""{(Any ? "" : RegexString)}"">";
         }
     }
 }
