aa0a83af59590133aff150f3074790eb4ea2022e
Ivan Kochurkin <kvanttt@gmail.com>
2018-08-18 00:17:29 +0300

    Add PathUtils with NormalizePath and NormalizeDirSeparator methods

---
+++ Sources/PT.PM.Common/PathUtils.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace PT.PM.Common.Utils
+{
+    public static class PathUtils
+    {
+        public const int MaxDirLength = 248 - 1;
+        public const int MaxPathLength = 260 - 1;
+
+        public static string NormalizePath(this string path, bool isDirectory = true, bool force = true)
+        {
+            if (CommonUtils.IsWindows && !CommonUtils.IsCoreApp && !path.StartsWith(@"\\?\") &&
+                (force || path.Length > (isDirectory ? MaxDirLength : MaxPathLength)))
+            {
+                if (path.StartsWith(@"\\"))
+                {
+                    return $@"\\?\UNC\{path.TrimStart('\\')}";
+                }
+
+                path = path.NormalizeDirSeparator();
+
+                return $@"\\?\{path}";
+            }
+
+            return path.NormalizeDirSeparator();
+        }
+
+        public static string NormalizeDirSeparator(this string path)
+        {
+            string notPlatformSeparator = CommonUtils.IsWindows ? "/" : "\\";
+
+            if (path.Contains(notPlatformSeparator))
+            {
+                return path.Replace(notPlatformSeparator, Path.DirectorySeparatorChar.ToString());
+            }
+
+            return path;
+        }
+    }
+}
