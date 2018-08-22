ff9e50fb450f3668f6207675e374c671310d9526
Ivan Kochurkin <kvanttt@gmail.com>
2018-08-22 14:09:38 +0300

    Denormalize long paths for DirectoryExt.Enumerate methods (correct displaying in log)

--- Sources/PT.PM.Common/PathUtils.cs
+++ Sources/PT.PM.Common/PathUtils.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace PT.PM.Common.Utils
 {
     public static class PathUtils
     {
+        private const string LongPrefix = @"\\?\";
+
         public const int MaxDirLength = 248 - 1;
         public const int MaxPathLength = 260 - 1;
 
@@ -15,16 +18,16 @@ namespace PT.PM.Common.Utils
         private static string NormalizePath(this string path, bool isDirectory = true, bool force = false)
         {
             if ((path.Length > (isDirectory ? MaxDirLength : MaxPathLength) || force) &&
-                CommonUtils.IsWindows && !CommonUtils.IsCoreApp && !path.StartsWith(@"\\?\"))
+                CommonUtils.IsWindows && !CommonUtils.IsCoreApp && !path.StartsWith(LongPrefix))
             {
                 if (path.StartsWith(@"\\"))
                 {
-                    return $@"\\?\UNC\{path.Substring(2)}";
+                    return $@"{LongPrefix}UNC\{path.Substring(2)}";
                 }
 
                 path = path.NormalizeDirSeparator();
 
-                return $@"\\?\{path}";
+                return $"{LongPrefix}{path}";
             }
 
             return path.NormalizeDirSeparator();
@@ -37,6 +40,16 @@ namespace PT.PM.Common.Utils
             if (path.Contains(notPlatformSeparator))
             {
                 return path.Replace(notPlatformSeparator, Path.DirectorySeparatorChar.ToString());
+            }
+
+            return path;
+        }
+
+        public static string DenormalizePath(this string path)
+        {
+            if (path.StartsWith(LongPrefix))
+            {
+                return path.Substring(LongPrefix.Length);
             }
 
             return path;
@@ -60,13 +73,41 @@ namespace PT.PM.Common.Utils
 
     public static class DirectoryExt
     {
-        public static IEnumerable<string> EnumerateFiles(string path, string searchPattern, SearchOption searchOption) => Directory.EnumerateFiles(path.NormalizeDirPath(true), searchPattern, searchOption);
+        public static IEnumerable<string> EnumerateFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(path.NormalizeDirPath(true), searchPattern, searchOption);
+            return files.Select(file => file.DenormalizePath());
+        }
 
-        public static IEnumerable<string> EnumerateFileSystemEntries(string path) => Directory.EnumerateFileSystemEntries(path.NormalizeDirPath(true));
+        public static IEnumerable<string> EnumerateFileSystemEntries(string path)
+        {
+            IEnumerable<string> fileSystemEntries = Directory.EnumerateFileSystemEntries(path.NormalizeDirPath(true));
+            return fileSystemEntries.Select(fileSystemEntry => fileSystemEntry.DenormalizePath());
+        }
 
-        public static string[] GetDirectories(string path) => Directory.GetDirectories(path.NormalizeDirPath(true));
+        public static string[] GetDirectories(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path.NormalizeDirPath(true));
 
-        public static string[] GetFiles(string path, string searchPattern, SearchOption searchOption) => Directory.GetFiles(path.NormalizeDirPath(true), searchPattern, searchOption);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                dirs[i] = dirs[i].DenormalizePath();
+            }
+
+            return dirs;
+        }
+
+        public static string[] GetFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            string[] files = Directory.GetFiles(path.NormalizeDirPath(true), searchPattern, searchOption);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = files[i].DenormalizePath();
+            }
+
+            return files;
+        }
 
         public static DirectoryInfo CreateDirectory(string path) => Directory.CreateDirectory(path.NormalizeDirPath());
 
