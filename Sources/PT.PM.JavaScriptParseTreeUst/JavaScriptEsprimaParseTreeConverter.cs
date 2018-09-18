453a562cb8c115735281d559a0b7d6125c222557
Ivan Kochurkin <kvanttt@gmail.com>
2018-09-18 12:17:47 +0300

    Add JavaScriptEsprimaParseTreeConverter

---
+++ Sources/PT.PM.JavaScriptParseTreeUst/JavaScriptEsprimaParseTreeConverter.cs
@@ -0,0 +1,27 @@
+﻿using PT.PM.Common;
+using PT.PM.Common.Nodes;
+using System;
+using System.Collections.Generic;
+
+namespace PT.PM.JavaScriptParseTreeUst
+{
+    public class JavaScriptEsprimaParseTreeConverter : IParseTreeToUstConverter
+    {
+        public ILogger Logger { get; set; } = DummyLogger.Instance;
+
+        public Language Language => JavaScript.Language;
+
+        public bool IsActive => false;
+
+        public HashSet<Language> AnalyzedLanguages { get; set; }
+
+        public RootUst ParentRoot { get; set; }
+
+        public RootUst Convert(ParseTree langParseTree)
+        {
+            var esprimParseTree = (JavaScriptEsprimaParseTree)langParseTree;
+
+            throw new NotImplementedException();
+        }
+    }
+}
