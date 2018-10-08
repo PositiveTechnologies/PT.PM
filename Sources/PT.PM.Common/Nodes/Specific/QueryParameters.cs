f417f7ba4c86120412b976f27479c6ab73c7a759
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2018-10-08 15:10:26 +0300

    Add specific nodes for SQL

---
+++ Sources/PT.PM.Common/Nodes/Specific/QueryParameters.cs
@@ -0,0 +1,28 @@
+﻿using PT.PM.Common.Nodes.Collections;
+using PT.PM.Common.Nodes.Expressions;
+using System;
+using System.Collections.Generic;
+
+namespace PT.PM.Common.Nodes.Specific
+{
+    public class QueryParameters : Expression
+    {
+        public Collection Parameters { get; set; }
+
+        public QueryParameters(IEnumerable<Expression> elements, TextSpan textSpan)
+            : base(textSpan)
+        {
+            Parameters = new Collection(elements ?? throw new ArgumentNullException(nameof(elements)));
+        }
+
+        public QueryParameters()
+        {
+        }
+
+        public override Expression[] GetArgs() => new Expression[] { this };
+
+        public override Ust[] GetChildren() => Parameters.Collection.ToArray();
+
+        public override string ToString() => string.Join(", ", Parameters.Collection);
+    }
+}
---
+++ Sources/PT.PM.Common/Nodes/Specific/SqlQueryStatement.cs
@@ -0,0 +1,47 @@
+﻿using PT.PM.Common.Nodes.Expressions;
+using PT.PM.Common.Nodes.Statements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PT.PM.Common.Nodes.Specific
+{
+    public class SqlQueryStatement : Statement
+    {
+        public Expression QueryCommand { get; set; }
+
+        public List<Expression> QueryElements { get; set; } = new List<Expression>();
+
+        public SqlQueryStatement(Expression queryKey, IEnumerable<Expression> queryElements,TextSpan textSpan)
+            : base(textSpan)
+        {
+            QueryCommand = queryKey ?? throw new ArgumentNullException(nameof(queryKey));
+            QueryElements = queryElements?.ToList() ?? QueryElements;
+        }
+
+        public SqlQueryStatement()
+        {
+        }
+
+        public override Ust[] GetChildren()
+        {
+            var children = new List<Ust>() { QueryCommand };
+            children.AddRange(QueryElements);
+            return children.ToArray();
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.Append(QueryCommand);
+            builder.Append(" ");
+            foreach (var expr in QueryElements)
+            {
+                builder.Append(expr);
+                builder.Append(" ");
+            }
+            return builder.ToString();
+        }
+    }
+}
