1209619b10a8c82d1875b6c2346cbc324cf2556e
Ivan Kochurkin <kvanttt@gmail.com>
2018-11-08 21:12:42 +0300

    Refactor JsonUtils

--- Sources/PT.PM.Common/Json/JsonUtils.cs
+++ Sources/PT.PM.Common/Json/JsonUtils.cs
@@ -8,32 +8,31 @@ namespace PT.PM.Common.Json
     {
         public static void LogError(this ILogger logger, CodeFile jsonFile, IJsonLineInfo jsonLineInfo, Exception ex, bool isError = true)
         {
-            string errorMessage = GenerateErrorPositionMessage(jsonFile, jsonLineInfo, out TextSpan errorTextSpan);
-            errorMessage += "; " + ex.FormatExceptionMessage();
+            string errorMessage = GenerateErrorPositionMessage(jsonFile, jsonLineInfo, out TextSpan errorTextSpan) +
+                                  "; " + ex.FormatExceptionMessage();
             LogErrorOrWarning(logger, jsonFile, isError, errorMessage, errorTextSpan);
         }
 
         public static void LogError(this ILogger logger, CodeFile jsonFile, IJsonLineInfo jsonLineInfo, string message, bool isError = true)
         {
-            string errorMessage = GenerateErrorPositionMessage(jsonFile, jsonLineInfo, out TextSpan errorTextSpan);
-            errorMessage += "; " + message;
+            string errorMessage = GenerateErrorPositionMessage(jsonFile, jsonLineInfo, out TextSpan errorTextSpan) +
+                                  "; " + message;
             LogErrorOrWarning(logger, jsonFile, isError, errorMessage, errorTextSpan);
         }
 
-        private static string GenerateErrorPositionMessage(CodeFile jsonFile, IJsonLineInfo jsonLineInfo, out TextSpan errorTextSpan)
+        private static string GenerateErrorPositionMessage(CodeFile jsonFile, IJsonLineInfo jsonLineInfo,
+            out TextSpan errorTextSpan)
         {
-            int errorLine = CodeFile.StartLine;
-            int errorColumn = CodeFile.StartColumn;
             if (jsonLineInfo != null)
             {
-                errorLine = jsonLineInfo.LineNumber;
-                errorColumn = jsonLineInfo.LinePosition;
+                int errorLine = jsonLineInfo.LineNumber;
+                int errorColumn = jsonLineInfo.LinePosition;
+                errorTextSpan = new TextSpan(jsonFile.GetLinearFromLineColumn(errorLine, errorColumn), 0);
+                return $"File position: {new LineColumnTextSpan(errorLine, errorColumn)}";
             }
-            errorTextSpan = new TextSpan(jsonFile.GetLinearFromLineColumn(errorLine, errorColumn), 0);
 
-            return jsonLineInfo != null
-                ? $"File position: {new LineColumnTextSpan(errorLine, errorColumn)}"
-                : "";
+            errorTextSpan = TextSpan.Zero;
+            return "";
         }
 
         private static void LogErrorOrWarning(this ILogger logger, CodeFile jsonFile, bool isError, string errorMessage, TextSpan errorTextSpan)
