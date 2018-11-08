ad9866d2005ace90bb668cbb64954d3b450a12fa
Dmitry Litovchenko <dmitry.litovchenko1@yandex.ru>
2018-11-08 18:23:06 +0300

    Add pattern and base Visitor helper (must be united with TSQl helper)

---
+++ Sources/PT.PM.MySqlParseTreeUst/MySqlConverterVisitorHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using PT.PM.AntlrUtils;
+using PT.PM.Common;
+using PT.PM.Common.Nodes;
+using PT.PM.Common.Nodes.Tokens;
+using PT.PM.Common.Nodes.Tokens.Literals;
+
+namespace PT.PM.SqlParseTreeUst
+{
+    public partial class MySqlAntlrConverter
+    {
+        public override Ust VisitTerminal(ITerminalNode node)
+        {
+            return ExtracLiteral(node.Symbol);
+        }
+
+        private Ust ExtracLiteral(IToken token)
+        {
+            string text = token.Text;
+            TextSpan textSpan = token.GetTextSpan();
+            Token result = null;
+
+            try
+            {
+                if (text == "*")
+                {
+                    result = new IdToken(text, textSpan);
+                }
+                else if (text.StartsWith("@"))
+                {
+                    result = new IdToken(text.Substring(1), textSpan);
+                }
+                else if (text.StartsWith("\"") || text.StartsWith("["))
+                {
+                    result = new IdToken(text.Substring(1, text.Length - 2), textSpan);
+                }
+                else if (text.EndsWith("'"))
+                {
+                    if (text.StartsWith("N"))
+                    {
+                        text = text.Substring(1);
+                    }
+                    text = text.Substring(1, text.Length - 2);
+                    result = new StringLiteral(text, textSpan);
+                }
+                else if (text.All(c => char.IsDigit(c)))
+                {
+                    result = new IntLiteral(long.Parse(text), textSpan);
+                }
+                else if (text.StartsWith("0X", StringComparison.OrdinalIgnoreCase))
+                {
+                    result = new IntLiteral(System.Convert.ToInt64(text.Substring(2), 16), textSpan);
+                }
+                else if (double.TryParse(text, out double floatValue))
+                {
+                    result = new FloatLiteral(floatValue, textSpan);
+                }
+            }
+            catch
+            {
+            }
+
+            if (result == null && (text.Any(c => char.IsLetterOrDigit(c) || c == '_')))
+            {
+                result = new IdToken(text, textSpan);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Sources/PT.PM.Patterns/MySqlPatterns.cs
@@ -0,0 +1,29 @@
+﻿using PT.PM.Common;
+using PT.PM.Matching;
+using PT.PM.Matching.Patterns;
+using PT.PM.SqlParseTreeUst;
+using System.Collections.Generic;
+
+namespace PT.PM.Patterns.PatternsRepository
+{
+    public partial class DefaultPatternRepository
+    {
+        public IEnumerable<PatternRoot> CreateMySqlPatterns()
+        {
+            var patterns = new List<PatternRoot>();
+            var mysql = MySql.Language;
+
+            patterns.Add(new PatternRoot
+            {
+                Key = patternIdGenerator.NextId(),
+                DebugInfo = "Weak Cryptographic Hash (MD2, MD4, MD5, RIPEMD-160, and SHA-1)",
+                Node = new PatternInvocationExpression
+                {
+                    Target = new PatternIdRegexToken("sha1"),
+                    Arguments = new PatternArgs(new PatternAny())
+                }
+            });
+            return patterns;
+        }
+    }
+}
