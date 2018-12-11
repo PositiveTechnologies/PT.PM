f90f88c6f0a1cab0da8c9d56c179145186a503a3
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2018-12-12 02:58:44 +0300

    Move ExtractLiteral to AntlrConverter base class

--- Sources/PT.PM.MySqlParseTreeUst/MySqlConverterVisitorHelper.cs
+++ Sources/PT.PM.MySqlParseTreeUst/MySqlConverterVisitorHelper.cs
@@ -1,12 +1,5 @@
-﻿using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
-using PT.PM.AntlrUtils;
-using PT.PM.Common;
+﻿using Antlr4.Runtime.Tree;
 using PT.PM.Common.Nodes;
-using PT.PM.Common.Nodes.Tokens;
-using PT.PM.Common.Nodes.Tokens.Literals;
-using System;
-using System.Linq;
 
 namespace PT.PM.SqlParseTreeUst
 {
@@ -15,61 +8,6 @@ namespace PT.PM.SqlParseTreeUst
         public override Ust VisitTerminal(ITerminalNode node)
         {
             return ExtractLiteral(node.Symbol);
-        }
-
-        private Ust ExtractLiteral(IToken token)
-        {
-            string text = token.Text;
-            TextSpan textSpan = token.GetTextSpan();
-            Token result = null;
-
-            try
-            {
-                if (text == "*")
-                {
-                    result = new IdToken(text, textSpan);
-                }
-                else if (text.StartsWith("@"))
-                {
-                    result = new IdToken(text.Substring(1), textSpan);
-                }
-                else if (text.StartsWith("\"") || text.StartsWith("["))
-                {
-                    result = new IdToken(text.Substring(1, text.Length - 2), textSpan);
-                }
-                else if (text.EndsWith("'"))
-                {
-                    if (text.StartsWith("N"))
-                    {
-                        text = text.Substring(1);
-                    }
-                    text = text.Substring(1, text.Length - 2);
-                    result = new StringLiteral(text, textSpan);
-                }
-                else if (text.All(c => char.IsDigit(c)))
-                {
-                    result = new IntLiteral(long.Parse(text), textSpan);
-                }
-                else if (text.StartsWith("0X", StringComparison.OrdinalIgnoreCase))
-                {
-                    result = new IntLiteral(System.Convert.ToInt64(text.Substring(2), 16), textSpan);
-                }
-                else if (double.TryParse(text, out double floatValue))
-                {
-                    result = new FloatLiteral(floatValue, textSpan);
-                }
-            }
-            catch
-            {
-                Logger.LogDebug($"Literal cannot be extracted from {nameof(token)} with symbol {text}");
-            }
-
-            if (result == null && (text.Any(c => char.IsLetterOrDigit(c) || c == '_')))
-            {
-                result = new IdToken(text, textSpan);
-            }
-
-            return result;
         }
     }
 }
--- Sources/PT.PM.TSqlParseTreeUst/TSqlConverterVisitorHelper.cs
+++ Sources/PT.PM.TSqlParseTreeUst/TSqlConverterVisitorHelper.cs
@@ -1,19 +1,16 @@
-﻿using PT.PM.AntlrUtils;
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using PT.PM.AntlrUtils;
 using PT.PM.Common;
 using PT.PM.Common.Nodes;
 using PT.PM.Common.Nodes.Collections;
 using PT.PM.Common.Nodes.Expressions;
-using PT.PM.Common.Nodes.Tokens;
 using PT.PM.Common.Nodes.Statements;
-using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
-using System;
+using PT.PM.Common.Nodes.Tokens;
+using PT.PM.TSqlParseTreeUst;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using PT.PM.Common.Nodes.Tokens.Literals;
-using PT.PM.TSqlParseTreeUst;
-using PT.PM.Common.Nodes.Specific;
 
 namespace PT.PM.SqlParseTreeUst
 {
@@ -79,60 +76,6 @@ namespace PT.PM.SqlParseTreeUst
                 }
             }
             return result.ToString();
-        }
-
-        private Token ExtractLiteral(IToken token)
-        {
-            string text = token.Text;
-            TextSpan textSpan = token.GetTextSpan();
-            Token result = null;
-
-            try
-            {
-                if (text == "*")
-                {
-                    result = new IdToken(text, textSpan);
-                }
-                else if (text.StartsWith("@"))
-                {
-                    result = new IdToken(text.Substring(1), textSpan);
-                }
-                else if (text.StartsWith("\"") || text.StartsWith("["))
-                {
-                    result = new IdToken(text.Substring(1, text.Length - 2), textSpan);
-                }
-                else if (text.EndsWith("'"))
-                {
-                    if (text.StartsWith("N"))
-                    {
-                        text = text.Substring(1);
-                    }
-                    text = text.Substring(1, text.Length - 2);
-                    result = new StringLiteral(text, textSpan);
-                }
-                else if (text.All(c => char.IsDigit(c)))
-                {
-                    result = new IntLiteral(long.Parse(text), textSpan);
-                }
-                else if (text.StartsWith("0X", StringComparison.OrdinalIgnoreCase))
-                {
-                    result = new IntLiteral(System.Convert.ToInt64(text.Substring(2), 16), textSpan);
-                }
-                else if (double.TryParse(text, out double floatValue))
-                {
-                    result = new FloatLiteral(floatValue, textSpan);
-                }
-            }
-            catch
-            {
-            }
-
-            if (result == null && (text.Any(c => char.IsLetterOrDigit(c) || c == '_')))
-            {
-                result = new IdToken(text, textSpan);
-            }
-
-            return result;
         }
     }
 }
