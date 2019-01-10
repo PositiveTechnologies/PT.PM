f1bf87ea818110beceddc27a29c28448c72350d8
Ivan Kochurkin <kvanttt@gmail.com>
2019-01-10 18:59:38 +0300

    Add NodeType enum with defined types (mapped with ust types)

---
+++ Sources/PT.PM.Common/Nodes/NodeType.cs
@@ -0,0 +1,101 @@
+namespace PT.PM.Common.Nodes
+{
+    public enum NodeType
+    {
+        RootUst,
+
+        // Collections
+        ArgsUst,
+        Collection,
+        EntitiesUst,
+
+        // Expressions
+        AnonymousMethodExpression,
+        AnonymousObjectExpression,
+        ArgumentExpression,
+        ArrayCreationExpression,
+        AssignmentExpression,
+        BinaryOperatorExpression,
+        CastExpression,
+        ConditionalExpression,
+        IndexerExpression,
+        InvocationExpression,
+        MemberReferenceExpression,
+        MultichildExpression,
+        ObjectCreateExpression,
+        UnaryOperatorExpression,
+        VariableDeclarationExpression,
+        WrapperExpression,
+        YieldExpression,
+        
+        // Generate Scope
+        NamespaceDeclaration,
+        TypeDeclaration,
+        UsingDeclaration,
+        
+        // Specific
+        ArrayPatternExpression,
+        AsExpression,
+        CheckedExpression,
+        CommaExpression,
+        DebuggerStatement,
+        FixedStatement,
+        LockStatement,
+        UnsafeStatement,
+        WithStatement,
+        
+        // Sql
+        QueryArgs,
+        SqlBlockStatement,
+        SqlQuery,
+        
+        // Statements
+        SwitchSection,
+        SwitchStatement,
+        CatchClause,
+        TryCatchStatement,
+        BlockStatement,
+        BreakStatement,
+        ContinueStatement,
+        DoWhileStatement,
+        EmptyStatement,
+        ExpressionStatement,
+        ForeachStatement,
+        ForStatement,
+        GotoStatement,
+        IfElseStatement,
+        LabelStatement,
+        ReturnStatement,
+        ThrowStatement,
+        TypeDeclarationStatement,
+        WhileStatement,
+        WrapperStatement,
+        
+        // Literals
+        BinaryOperatorLiteral,
+        BooleanLiteral,
+        CommentLiteral,
+        FloatLiteral,
+        InOutModifierLiteral,
+        IntLiteral,
+        ModifierLiteral,
+        NullLiteral,
+        StringLiteral,
+        TypeTypeLiteral,
+        UnaryOperatorLiteral,
+        
+        // Tokens
+        BaseReferenceToken,
+        IdToken,
+        ThisReferenceToken,
+        TypeToken,
+        
+        // Type Members
+        ConstructorDeclaration,
+        FieldDeclaration,
+        MethodDeclaration,
+        ParameterDeclaration,
+        PropertyDeclaration,
+        StatementDeclaration,
+    }
+}
