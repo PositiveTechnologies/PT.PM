2cb776566eb802b1bf6649508fd7782261ff1304
Ivan Kochurkin <kvanttt@gmail.com>
2019-01-11 00:47:01 +0300

    Add RootUstMessagePackSerializer that implements ISerializer

---
+++ Sources/PT.PM.Common/MessagePack/RootUstMessagePackSerializer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using MessagePack;
+using PT.PM.Common.Files;
+using PT.PM.Common.Nodes;
+
+namespace PT.PM.Common.MessagePack
+{
+    public class RootUstMessagePackSerializer : ISerializer
+    {
+        public ILogger Logger { get; set; } = DummyLogger.Instance;
+
+        public bool LineColumnTextSpans { get; set; }
+
+        public byte[] Serialize(RootUst rootUst)
+        {
+            var writerResolver = UstMessagePackResolver.CreateWriter(rootUst.SourceCodeFile, LineColumnTextSpans, Logger);
+            return MessagePackSerializer.Serialize(rootUst, writerResolver);
+        }
+
+        public RootUst Deserialize(BinaryFile serializedFile, HashSet<IFile> sourceFiles, Action<(IFile, TimeSpan)> readCodeFileAction)
+        {
+            var readerResolver =
+                UstMessagePackResolver.CreateReader(serializedFile, LineColumnTextSpans, sourceFiles, readCodeFileAction, Logger);
+            var result = MessagePackSerializer.Deserialize<RootUst>(serializedFile.Data, readerResolver);
+            result.FillAscendants();
+            return result;
+        }
+    }
+}
