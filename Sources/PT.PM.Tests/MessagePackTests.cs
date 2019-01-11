c5bcaeda98637abe44fd0328888c8962df46d582
Ivan Kochurkin <kvanttt@gmail.com>
2019-01-11 22:40:19 +0300

    Add Serialize_MsgPack_DamagedFileCatchErrors unit test for checking errors handling

--- Sources/PT.PM.Tests/MessagePackTests.cs
+++ Sources/PT.PM.Tests/MessagePackTests.cs
@@ -27,51 +27,62 @@ namespace PT.PM.Tests
             CheckMsgPackSerialization("empty-try-catch.php", true);
         }
 
+        [Test]
+        public void Serialize_MsgPack_DamagedFileCatchErrors()
+        {
+            CheckMsgPackSerialization("empty-try-catch.php", damaged: true);
+        }
+
         private static void CheckMsgPackSerialization(string inputFileName, bool linearTextSpans = false,
-            bool checkPatternSerialization = false)
+            bool damaged = false)
         {
             string path = Path.Combine(TestUtility.TestsDataPath, inputFileName);
             string ext = SerializationFormat.MsgPack.GetExtension();
 
             // Serialization
             string[] files = File.Exists(path)
-                           ? new [] { path }
-                           : Directory.GetFiles(path);
+                ? new[] {path}
+                : Directory.GetFiles(path);
             var codeRepository = new FileCodeRepository(files);
 
             var logger = new LoggerMessageCounter();
             var workflow = new Workflow(codeRepository)
             {
-                DumpStages = new HashSet<Stage>() { Stage.Ust },
+                DumpStages = new HashSet<Stage> {Stage.Ust},
                 DumpDir = TestUtility.TestsOutputPath,
                 SerializationFormat = SerializationFormat.MsgPack,
                 LineColumnTextSpans = !linearTextSpans,
                 Stage = Stage.Ust,
-                IsDumpPatterns = checkPatternSerialization,
                 Logger = logger
             };
             WorkflowResult result = workflow.Process();
 
             Assert.AreEqual(0, logger.ErrorCount, logger.ErrorsString);
 
+            int errorOffset = 106;
+            byte errorValue = 123;
+
             var serializedFiles = new List<string>();
             foreach (string file in files)
             {
                 string shortFileName = Path.GetFileName(file) + ".ust." + ext;
-                string jsonFile = Path.Combine(TestUtility.TestsOutputPath, shortFileName);
-                serializedFiles.Add(jsonFile);
+                string serializedFile = Path.Combine(TestUtility.TestsOutputPath, shortFileName);
+
+                if (damaged)
+                {
+                    byte[] bytes = File.ReadAllBytes(serializedFile);
+                    bytes[errorOffset] = errorValue;
+                    File.WriteAllBytes(serializedFile, bytes);
+                }
+
+                serializedFiles.Add(serializedFile);
             }
 
             // Deserialization
             var newLogger = new LoggerMessageCounter();
             var newCodeRepository = new FileCodeRepository(serializedFiles, format: SerializationFormat.MsgPack);
 
-            var newPatternsRepository = checkPatternSerialization
-                ? new JsonPatternsRepository(File.ReadAllText(Path.Combine(TestUtility.TestsOutputPath, "patterns." + ext)))
-                : inputFileName == "MultiTextSpan"
-                ? (IPatternsRepository)new DslPatternRepository("a", "php")
-                : new DefaultPatternRepository();
-            var newWorkflow = new Workflow(newCodeRepository, newPatternsRepository)
+            var newWorkflow = new Workflow(newCodeRepository, new DefaultPatternRepository())
             {
                 StartStage = Stage.Ust,
                 SerializationFormat = SerializationFormat.MsgPack,
@@ -80,29 +91,23 @@ namespace PT.PM.Tests
             };
             newWorkflow.Process();
 
+            if (damaged)
+            {
+                Assert.AreEqual(1, newLogger.ErrorCount);
+                Assert.IsTrue(newLogger.ErrorsString.Contains(errorValue.ToString()));
+                Assert.IsTrue(newLogger.ErrorsString.Contains(errorOffset.ToString()));
+                return;
+            }
+
             Assert.AreEqual(0, newLogger.ErrorCount, newLogger.ErrorsString);
             Assert.GreaterOrEqual(newLogger.Matches.Count, 1);
 
-            if (inputFileName == "MultiTextSpan")
+            var match = (MatchResult) newLogger.Matches[0];
+            using (var sourceFilesEnumerator = result.SourceFiles.GetEnumerator())
             {
-                /*var matchResult = (MatchResult)logger.Matches[1];
-                Assert.AreEqual(2, matchResult.TextSpans.Length);
-
-                LineColumnTextSpan actualOriginTextSpan = originFile.GetLineColumnTextSpan(matchResult.TextSpans[1]);
-                Assert.AreEqual(lcOriginTextSpan, actualOriginTextSpan);
-
-                LineColumnTextSpan actualPreprocessedTextSpan = preprocessedFile.GetLineColumnTextSpan(matchResult.TextSpans[0]);
-                Assert.AreEqual(lcPreprocessedTextSpan, actualPreprocessedTextSpan);*/
-            }
-            else
-            {
-                var match = (MatchResult)newLogger.Matches[0];
-                using (var sourceFilesEnumerator = result.SourceFiles.GetEnumerator())
-                {
-                    sourceFilesEnumerator.MoveNext();
-                    var firstFile = (TextFile)sourceFilesEnumerator.Current;
-                    Assert.AreEqual(new LineColumnTextSpan(2, 1, 3, 25), firstFile.GetLineColumnTextSpan(match.TextSpan));
-                }
+                sourceFilesEnumerator.MoveNext();
+                var firstFile = (TextFile) sourceFilesEnumerator.Current;
+                Assert.AreEqual(new LineColumnTextSpan(2, 1, 3, 25), firstFile.GetLineColumnTextSpan(match.TextSpan));
             }
         }
     }
