8c01d415dce68c28a3eb6a959de2d3a5a301caff
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2019-01-28 16:24:20 +0300

    Update Test for Tuple converting check

--- Sources/PT.PM.CSharpParseTreeUst.Tests/CSharpConverterTests.cs
+++ Sources/PT.PM.CSharpParseTreeUst.Tests/CSharpConverterTests.cs
@@ -37,5 +37,13 @@ namespace PT.PM.CSharpParseTreeUst.Tests
             string fileName = Path.Combine(TestUtility.TestsDataPath, "ArrayExamples.cs");
             TestUtility.CheckFile(fileName, Stage.Ust, out RootUst ust);
         }
+
+        [Test]
+        public void Check_Tuple_Initializations()
+        {
+            string fileName = Path.Combine(TestUtility.TestsDataPath, "ValueTuple.cs");
+            var matches = TestUtility.CheckFile(fileName, Stage.Match, out RootUst ust);
+            Assert.AreEqual(1, matches.Count());
+        }
     }
 }
--- Sources/PT.PM.CSharpParseTreeUst.Tests/Data/ValueTuple.cs
+++ Sources/PT.PM.CSharpParseTreeUst.Tests/Data/ValueTuple.cs
@@ -3,5 +3,7 @@
     static void Main()
     {
         var tuple = (Password: "hardcoded", 1, 1.5, "otherstring");
+        (string password, int x) = ("hardcoded", 1);
+        (string password, int x) tuple = ("hardcoded", 1);
     }
 }
