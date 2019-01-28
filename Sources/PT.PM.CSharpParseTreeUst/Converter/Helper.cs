beb23ab2d7ac03042975a4c04b0a9fc0aea4568e
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2019-01-28 15:47:14 +0300

    Add different handlers for different C# tuple initialization cases

--- Sources/PT.PM.CSharpParseTreeUst/Converter/Helper.cs
+++ Sources/PT.PM.CSharpParseTreeUst/Converter/Helper.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using PT.PM.Common.Nodes;
-using PT.PM.Common.Nodes.Tokens;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using PT.PM.Common.Exceptions;
+using PT.PM.Common.Nodes;
+using PT.PM.Common.Nodes.Expressions;
+using PT.PM.Common.Nodes.Statements;
+using PT.PM.Common.Nodes.Tokens;
 using PT.PM.Common.Nodes.Tokens.Literals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace PT.PM.CSharpParseTreeUst.RoslynUstVisitor
@@ -33,6 +38,92 @@ namespace PT.PM.CSharpParseTreeUst.RoslynUstVisitor
                 return new TypeToken(idToken.Id, idToken.TextSpan);
 
             return null;
+        }
+
+        private Ust HandleTupleStatement(VariableDeclarationSyntax node)
+        {
+            List<AssignmentExpression> variables =
+                node.Variables.Select(v => (AssignmentExpression)VisitAndReturnNullIfError(v)).ToList();
+
+            var typeNode = (TupleTypeSyntax)node.Type;
+            var textSpan = node.GetTextSpan();
+
+            for (int i = 0; i < typeNode.Elements.Count; i++)
+            {
+                TupleElementSyntax typeElement = typeNode.Elements[i];
+                if (typeElement.Identifier == null)
+                {
+                    continue;
+                }
+
+                variables.ForEach(variable =>
+                {
+                    var tupleTypeElementMemRef = new MemberReferenceExpression
+                    {
+                        Target = new IdToken(((IdToken)variable.Left).Id, variable.Left.TextSpan),
+                        Name = ConvertId(typeElement.Identifier)
+                    };
+
+                    var right = (TupleCreateExpression)variable?.Right;
+                    if (right == null)
+                    {
+                        return;
+                    }
+                    var oldName = ((AssignmentExpression)right.Initializers[i]).Left
+                        as MemberReferenceExpression;
+                    var newName = CopyMemberReference(oldName);
+                    right.Initializers.Add(new AssignmentExpression
+                    {
+                        Left = tupleTypeElementMemRef,
+                        Right = newName,
+                        TextSpan = textSpan
+                    });
+                });
+            }
+            var declaration = new VariableDeclarationExpression(null, variables, textSpan);
+            return new ExpressionStatement(declaration);
+        }
+
+        private Ust HandleTupleStatement(VariableDeclaratorSyntax node)
+        {
+            var tuple = (TupleCreateExpression)base.Visit(node.Initializer.Value);
+            var idText = node.Identifier.ValueText;
+            var idTextSpan = node.Identifier.GetTextSpan();
+
+            tuple.Initializers.ForEach(init =>
+            {
+                var assignment = (AssignmentExpression)init;
+                assignment.Left = new MemberReferenceExpression(
+                    new IdToken(idText, idTextSpan), assignment.Left, assignment.Left.TextSpan);
+
+                if(assignment.Right is MemberReferenceExpression memref  
+                && memref.Target == null) // Memref with empty target created in tuple expression visitor
+                {
+                    memref.Target = new IdToken(idText, idTextSpan);
+                }
+            });
+
+            return new AssignmentExpression(new IdToken(idText, idTextSpan), tuple, node.GetTextSpan());
+        }
+
+        private MemberReferenceExpression CopyMemberReference(MemberReferenceExpression memRef)
+        {
+            Expression target = null;
+            if (memRef.Target is MemberReferenceExpression memRefTarget)
+            {
+                target = CopyMemberReference(memRefTarget);
+            }
+            else
+            {
+                target = new IdToken(((IdToken)memRef.Target).Id, memRef.Target.TextSpan);
+            }
+
+            return new MemberReferenceExpression
+            {
+                Target = target,
+                Name = new IdToken(((IdToken)memRef.Name).Id, memRef.Name.TextSpan),
+                TextSpan = memRef.TextSpan
+            };
         }
 
         protected Ust VisitAndReturnNullIfError(SyntaxNode node)
