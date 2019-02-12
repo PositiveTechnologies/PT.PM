d51a014b193249ddcc4618ec8091816bdc9ea900
Ivan Kochurkin <kvanttt@gmail.com>
2019-02-12 12:51:29 +0300

    Increment version (1.6.7 -> 1.6.8); update Copyright year

--- Sources/AssemblyInfoCommon.cs
+++ Sources/AssemblyInfoCommon.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Positive Technologies")]
 [assembly: AssemblyProduct("PT.PM")]
-[assembly: AssemblyCopyright("Copyright © Positive Technologies 2015-2018")]
+[assembly: AssemblyCopyright("Copyright © Positive Technologies 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.8.0")]
