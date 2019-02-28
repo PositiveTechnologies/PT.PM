0b2312edb69c80b25ab0c3cc52aae994e6f7600b
Ivan Kochurkin <kvanttt@gmail.com>
2019-02-28 19:16:26 +0300

    Trim large input in SqlDialectsAntlrLexer

--- Sources/PT.PM/LanguageDetectors/SqlDialectsAntlrLexer.cs
+++ Sources/PT.PM/LanguageDetectors/SqlDialectsAntlrLexer.cs
@@ -1,13 +1,16 @@
 using Antlr4.Runtime;
 using PT.PM.AntlrUtils;
 using PT.PM.Common;
+using PT.PM.Common.Files;
 using PT.PM.LanguageDetectors;
 
 namespace PT.PM
 {
     public class SqlDialectsAntlrLexer : AntlrLexer
     {
-        public override Language Language => Language.PlSql;
+        public static int MaxCharsCount { get; set; } = 50000;
+
+        public override Language Language => Language.Sql;
 
         public override CaseInsensitiveType CaseInsensitiveType => CaseInsensitiveType.UPPER;
 
@@ -15,7 +18,30 @@ namespace PT.PM
 
         public override IVocabulary Vocabulary => SqlDialectsLexer.DefaultVocabulary;
 
-        public override Lexer InitLexer(ICharStream inputStream) =>
-            new SqlDialectsLexer(inputStream);
+        public override Lexer InitLexer(ICharStream inputStream) => new SqlDialectsLexer(inputStream);
+
+        protected override string PreprocessText(TextFile file)
+        {
+            string data = file.Data;
+            string shortenedData;
+
+            if (data.Length > MaxCharsCount)
+            {
+                // Remove trailing not spaces
+                int lastInd = MaxCharsCount - 1;
+                while (lastInd > 0 && !char.IsWhiteSpace(data[lastInd]))
+                {
+                    lastInd--;
+                }
+
+                shortenedData = data.Remove(lastInd + 1);
+            }
+            else
+            {
+                shortenedData = data;
+            }
+
+            return base.PreprocessText(new TextFile(shortenedData));
+        }
     }
 }
