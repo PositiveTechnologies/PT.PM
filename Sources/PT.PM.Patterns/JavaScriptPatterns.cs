46c5a83ac5ffa5b5b91b10693df1907702f9c0b7
Alexander Tchitchigin <sad.ronin@gmail.com>
2019-03-05 17:20:07 +0300

    Server Side Injection patterns.

--- Sources/PT.PM.Patterns/JavaScriptPatterns.cs
+++ Sources/PT.PM.Patterns/JavaScriptPatterns.cs
@@ -76,6 +76,102 @@ namespace PT.PM.Patterns.PatternsRepository
                 }
             });
 
+            // Node.js Patterns
+            var nodeEP = new PatternOr
+            (
+                new PatternIdToken("req"),
+                new PatternMemberReferenceExpression
+                {
+                    Target = new PatternMemberReferenceExpression
+                    {
+                        Target = new PatternIdToken("req"),
+                        Name = new PatternIdToken("query")
+                    },
+                    Name = new PatternAny()
+                },
+                new PatternMemberReferenceExpression
+                {
+                    Target = new PatternIdToken("req"),
+                    Name = new PatternIdToken("query")
+                },
+                new PatternMemberReferenceExpression
+                {
+                    Target = new PatternMemberReferenceExpression
+                    {
+                        Target = new PatternIdToken("req"),
+                        Name = new PatternIdToken("body")
+                    },
+                    Name = new PatternAny()
+                },
+                new PatternMemberReferenceExpression
+                {
+                    Target = new PatternIdToken("req"),
+                    Name = new PatternIdToken("body")
+                },
+                new PatternMemberReferenceExpression
+                {
+                    Target = new PatternMemberReferenceExpression
+                    {
+                        Target = new PatternIdToken("req"),
+                        Name = new PatternIdToken("param")
+                    },
+                    Name = new PatternAny()
+                },
+                new PatternMemberReferenceExpression
+                {
+                    Target = new PatternIdToken("req"),
+                    Name = new PatternIdToken("param")
+                }
+            );
+
+            patterns.Add(new PatternRoot
+            {
+                Key = patternIdGenerator.NextId(),
+                DebugInfo = "ServerSideInjectionEval",
+                Languages = new HashSet<Language> { Language.JavaScript },
+                Node = new PatternInvocationExpression
+                {
+                    Target = new PatternIdToken("eval"),
+                    Arguments = new PatternArgs(nodeEP)
+                }
+            });
+
+            patterns.Add(new PatternRoot
+            {
+                Key = patternIdGenerator.NextId(),
+                DebugInfo = "ServerSideInjectionSetTimeout",
+                Languages = new HashSet<Language> { Language.JavaScript },
+                Node = new PatternInvocationExpression
+                {
+                    Target = new PatternIdToken("setTimeout"),
+                    Arguments = new PatternArgs(nodeEP, new PatternMultipleExpressions()) // setTimeout(callback, delay[, ...args])
+                }
+            });
+
+            patterns.Add(new PatternRoot
+            {
+                Key = patternIdGenerator.NextId(),
+                DebugInfo = "ServerSideInjectionSetInterval",
+                Languages = new HashSet<Language> { Language.JavaScript },
+                Node = new PatternInvocationExpression
+                {
+                    Target = new PatternIdToken("setInterval"),
+                    Arguments = new PatternArgs(nodeEP, new PatternMultipleExpressions()) // setInterval(callback, delay[, ...args])
+                }
+            });
+
+            patterns.Add(new PatternRoot
+            {
+                Key = patternIdGenerator.NextId(),
+                DebugInfo = "ServerSideInjectionFunction",
+                Languages = new HashSet<Language> { Language.JavaScript },
+                Node = new PatternObjectCreateExpression
+                {
+                    Type = new PatternIdToken("Function"),
+                    Arguments = new PatternArgs(new PatternMultipleExpressions(), nodeEP) // new Function([arg1, ... , argN], body)
+                }
+            });
+
             return patterns;
         }
     }
---
+++ Sources/PT.PM.Tests/Data/NodeJS/ssi.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+
+app.get("/eval", function(req, res) {
+	eval(req.query);
+	eval(req.query.func);
+});
+
+app.get("/interval", function(req, res) {
+	setInterval(req.body, 100);
+	setInterval(req.body.code, 100);
+	setInterval(req.query.func, 100);
+});
+
+app.get("/timeout", function(req, res) {
+	setTimeout(req.param, 100);
+	setTimeout(req.body.code, 100);
+	setTimeout(req.param.func, 100);
+});
+
+app.get("/function", function(req, res) {
+	new Function(req.query);
+	new Function("x", "y", req.query.func);
+});
+
+app.listen(3000);
