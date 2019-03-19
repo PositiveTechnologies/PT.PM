5a73873c0ea98c86a57a4672b002a8d272223eb2
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2019-03-19 13:10:28 +0300

    Add else to CatchStatement

--- Sources/PT.PM.Common/Nodes/Statements/TryCatchFinally/TryCatchStatement.cs
+++ Sources/PT.PM.Common/Nodes/Statements/TryCatchFinally/TryCatchStatement.cs
@@ -10,9 +10,12 @@ namespace PT.PM.Common.Nodes.Statements.TryCatchFinally
         public BlockStatement TryBlock { get; set; }
 
         [Key(UstFieldOffset + 1)]
-        public List<CatchClause> CatchClauses { get; set; }
+        public List<CatchClause> CatchClauses { get; set; } = new List<CatchClause>();
 
         [Key(UstFieldOffset + 2)]
+        public BlockStatement ElseBlock { get; set; }
+
+        [Key(UstFieldOffset + 3)]
         public BlockStatement FinallyBlock { get; set; }
 
         public TryCatchStatement(BlockStatement tryBlock, TextSpan textSpan)
@@ -30,22 +33,34 @@ namespace PT.PM.Common.Nodes.Statements.TryCatchFinally
             var result = new List<Ust>();
             result.Add(TryBlock);
             if (CatchClauses != null)
+            {
                 result.AddRange(CatchClauses);
+            }
+            if (ElseBlock != null)
+            {
+                result.Add(ElseBlock);
+            }
             if (FinallyBlock != null)
+            {
                 result.Add(FinallyBlock);
+            }
             return result.ToArray();
         }
 
         public override string ToString()
         {
-            var result = $"try {{{TryBlock}}}\n";
+            var result = $"try {TryBlock}\n";
             if (CatchClauses?.Count > 0)
             {
                 result += string.Join("\n", CatchClauses);
             }
+            if (ElseBlock != null)
+            {
+                result += $"else\n\n{ElseBlock.ToStringWithTrailNL()}";
+            }
             if (FinallyBlock != null)
             {
-                result += $"finally\n{{\n{FinallyBlock.ToStringWithTrailNL()}}}";
+                result += $"finally\n\n{FinallyBlock.ToStringWithTrailNL()}";
             }
             return result;
         }
