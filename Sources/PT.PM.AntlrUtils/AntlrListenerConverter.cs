7c41b757e60c2b34f22afb9c5a8a6345da90e5a3
Ivan Kochurkin <kvanttt@gmail.com>
2019-05-28 20:58:49 +0300

    Replace `Stack<int> ChildCount` and `List<Ust> resultNodes` on `Stack<List<Ust>> childNodes`; Make AntlrListenerConverter abstract, refactor

--- Sources/PT.PM.AntlrUtils/AntlrListenerConverter.cs
+++ Sources/PT.PM.AntlrUtils/AntlrListenerConverter.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Antlr4.Runtime;
@@ -15,34 +14,28 @@ namespace PT.PM.AntlrUtils
     /// Generates UST node at each `ExitEveryRule` method
     /// If needed, use concrete Listener with overriden methods for certain contexts
     /// </summary>
-    public class AntlrListenerConverter : IParseTreeListener, ILoggable
+    public abstract class AntlrListenerConverter : IParseTreeListener, ILoggable
     {
-        // We should store children count for every ascendant node and process such number in ProcessChildren
-        // After processing, such nodes should be removed from `resultNodes`
-        protected Stack<int> ChildCount { get; } = new Stack<int>(64);
-
-        // Stack of created children
-        protected readonly List<Ust> resultNodes = new List<Ust>();
-
+        private readonly Stack<List<Ust>> childNodes = new Stack<List<Ust>>();
+        private readonly AntlrConvertHelper convertHelper;
         protected readonly RootUst root;
-        protected readonly AntlrConvertHelper convertHelper;
 
         public ILogger Logger { get; set; } = DummyLogger.Instance;
 
-        public AntlrListenerConverter(TextFile sourceFile, AntlrParserBase antlrParser)
+        protected AntlrListenerConverter(TextFile sourceFile, AntlrParserBase antlrParser)
         {
             root = new RootUst(sourceFile, antlrParser.Language);
             convertHelper = new AntlrConvertHelper(root, antlrParser);
-            ChildCount.Push(0);
+            childNodes.Push(new List<Ust>());
         }
 
         /// <summary>
         /// Calls at the end of conversion process.
-        /// </summary>6
+        /// </summary>
         public RootUst Complete()
         {
             ProcessChildren();
-            root.Node = resultNodes[resultNodes.Count - 1];
+            root.Node = childNodes.Pop()[0];
             root.FillAscendants(); // TODO: should be filled during conversion
             Clear();
             return root;
@@ -50,70 +43,42 @@ namespace PT.PM.AntlrUtils
 
         public void Clear()
         {
-            resultNodes.Clear();
-            ChildCount.Clear();
-            ChildCount.Push(0);
+            childNodes.Clear();
+            childNodes.Push(new List<Ust>());
         }
 
-        /// <summary>
-        /// Saves child count for every context
-        /// </summary>
         public void EnterEveryRule(ParserRuleContext ctx)
         {
-            ChildCount.Push(ChildCount.Pop() + 1);
-            ChildCount.Push(0);
+            childNodes.Push(new List<Ust>());
         }
 
         public virtual void ExitEveryRule(ParserRuleContext ctx)
         {
-            int childCount = ChildCount.Peek();
-
-            if (childCount != 1)
-            {
-                Ust result = ProcessChildren();
-                RemoveAndAdd(result);
-            }
+            RemoveAndAdd(ProcessChildren());
         }
 
         public void VisitTerminal(ITerminalNode node)
         {
-            ChildCount.Push(ChildCount.Pop() + 1);
-
-            resultNodes.Add(convertHelper.Convert(node));
+            childNodes.Peek().Add(convertHelper.Convert(node));
         }
 
         public void VisitErrorNode(IErrorNode node)
         {
-            ChildCount.Push(ChildCount.Pop() + 1);
-
-            ReadOnlySpan<char> span = node.Symbol.ExtractSpan(out TextSpan textSpan);
-            resultNodes.Add(convertHelper.ConvertToken(span, textSpan));
-        }
-
-        protected int GetKeywordIndex(string keywordValue, int childCount)
-        {
-            for (int i = resultNodes.Count - childCount; i < resultNodes.Count; i++)
-            {
-                if (resultNodes[i] is Keyword keyword && keyword.Substring.EqualsIgnoreCase(keywordValue))
-                {
-                    return i - (resultNodes.Count - childCount);
-                }
-            }
-
-            return -1;
+            childNodes.Peek().Add(convertHelper.Convert(node));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected int TryGetChildOfType<T>(out T child)
             where T : Ust
         {
-            int childCount = ChildCount.Peek();
-            for (int i = resultNodes.Count - childCount; i < resultNodes.Count; i++)
+            List<Ust> children = childNodes.Peek();
+
+            for (int i = 0; i < childNodes.Count; i++)
             {
-                if (resultNodes[i] is T ust)
+                if (children[i] is T ust)
                 {
                     child = ust;
-                    return i - (resultNodes.Count - childCount);
+                    return i;
                 }
             }
 
@@ -122,40 +87,42 @@ namespace PT.PM.AntlrUtils
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected List<Ust> GetChildren() => childNodes.Peek();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected bool CheckKeyword(string keywordValue, int index)
             => GetChild(index) is Keyword keyword && keyword.Substring.EqualsIgnoreCase(keywordValue);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected bool CheckKeywordFromEnd(string keywordValue, int index)
-            => GetChildFromEnd(index) is Keyword keyword && keyword.Substring.EqualsIgnoreCase(keywordValue);
+        protected Ust GetChild(int index) => childNodes.Peek()[index];
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected Ust GetChild(int index) => resultNodes[resultNodes.Count - ChildCount.Peek() + index];
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected Ust GetChildFromEnd(int index) => resultNodes[resultNodes.Count - 1 - index];
+        protected Ust GetChildFromEnd(int index)
+        {
+            List<Ust> nodes = childNodes.Peek();
+            return nodes[nodes.Count - 1 - index];
+        }
 
         protected Ust ProcessChildren()
         {
             Ust result;
 
-            int childCount = ChildCount.Peek();
-            if (childCount == 0)
+            List<Ust> nodes = childNodes.Peek();
+            if (nodes.Count == 0)
             {
                 result = null;
             }
-            else if (childCount == 1)
+            else if (nodes.Count == 1)
             {
-                result = resultNodes[resultNodes.Count - 1];
+                result = nodes[0];
             }
             else
             {
-                var resultCollection = new List<Ust>(childCount);
+                var resultCollection = new List<Ust>(nodes.Count);
 
                 // Extract and process previous nodes in common, left-to-right order
-                for (int i = resultNodes.Count - childCount; i < resultNodes.Count; i++)
+                foreach (Ust resultNode in nodes)
                 {
-                    Ust resultNode = resultNodes[i];
                     if (resultNode != null)
                     {
                         resultCollection.Add(resultNode);
@@ -172,28 +139,22 @@ namespace PT.PM.AntlrUtils
 
         protected void RemoveAndAdd(Ust result)
         {
-            int removeCount = ChildCount.Pop();
-            if (removeCount == 0)
+            List<Ust> nodes = childNodes.Pop();
+            if (nodes.Count == 0)
             {
                 return;
             }
 
-            for (int i = resultNodes.Count - removeCount; i < resultNodes.Count; i++)
+            foreach (Ust node in nodes)
             {
-                Ust resultNode = resultNodes[i];
-                if (resultNode != null)
+                if (node != null)
                 {
-                    resultNode.Root = root;
-                    resultNode.Parent = result;
+                    node.Parent = result;
+                    node.Root = root;
                 }
             }
 
-            // Remove processed children except the first one
-            int countToRemove = removeCount - 1;
-            resultNodes.RemoveRange(resultNodes.Count - countToRemove, countToRemove);
-
-            // Replace it with the new value
-            resultNodes[resultNodes.Count - 1] = result;
+            childNodes.Peek().Add(result);
         }
     }
 }
